21cfab74b1bf51fc81d1b62337b02b8694f667ea
Raghad717 <r4gh4dalmalki@gmail.com>
2026-02-18 14:32:41 +0300

    Update facade.py

--- part2/app/services/facade.py
+++ part2/app/services/facade.py
@@ -37,47 +37,26 @@ class HBnBFacade:
         return self.users.delete(user_id)
 
     # ---------------- Places ----------------
+    
     def create_place(self, data):
-        owner = self.users.get(data.get("owner_id"))
-        if not owner:
-            return None
-        place = Place(**data)
-        owner.places.append(place.id)
-        self.users.update(owner.id, {"places": owner.places})
-        return self.places.add(place)
+    owner = self.users.get(data.get("owner_id"))
+    if not owner:
+        return None
+    
+    place_data = {
+        "title": data.get("title"),
+        "description": data.get("description", ""),
+        "price_per_night": data.get("price_per_night", 0.0),
+        "latitude": data.get("latitude"),
+        "longitude": data.get("longitude"),
+        "owner_id": data.get("owner_id")
+    }
 
-    def get_place(self, place_id):
-        return self.places.get(place_id)
-
-    def get_all_places(self):
-        return self.places.get_all()
-
-    def update_place(self, place_id, data):
-        return self.places.update(place_id, data)
-
-    def delete_place(self, place_id):
-        return self.places.delete(place_id)
-
-    def add_amenity_to_place(self, place_id, amenity_id):
-        place = self.places.get(place_id)
-        amenity = self.amenities.get(amenity_id)
-        if not place or not amenity:
-            return False
-
-        if not hasattr(place, "amenities"):
-            place.amenities = []
-        if amenity_id not in place.amenities:
-            place.amenities.append(amenity_id)
-
-        if not hasattr(amenity, "place_ids"):
-            amenity.place_ids = []
-        if place_id not in amenity.place_ids:
-            amenity.place_ids.append(place_id)
-
-        self.places.update(place_id, {"amenities": place.amenities})
-        self.amenities.update(amenity_id, {"place_ids": amenity.place_ids})
-        return True
-
+    place = Place(**place_data)
+    owner.places.append(place.id)
+    self.users.update(owner.id, {"places": owner.places})
+    return self.places.add(place)
+    
     # ---------------- Reviews ----------------
     def create_review(self, data):
         user = self.users.get(data.get("user_id"))
