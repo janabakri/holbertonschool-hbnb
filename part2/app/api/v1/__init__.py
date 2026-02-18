857ed742690bb96425ae582868a887445ca9c599
jana <bkeyh382@gmail.com>
2026-02-18 14:03:52 +0300

    Set up API v1 with Flask-RESTx and namespaces
    Initialize API v1 with Flask-RESTx and add namespaces.

--- part2/app/api/v1/__init__.py
+++ part2/app/api/v1/__init__.py
@@ -1,4 +1,32 @@
 """
 API v1 package initialization
 """
-__version__ = '1.0.0'
+from flask_restx import Api
+from flask import Blueprint
+
+# Create Blueprint for API v1
+bp = Blueprint('api', __name__, url_prefix='/api/v1')
+
+# Create Api instance
+api = Api(
+    bp,
+    version='1.0',
+    title='HBnB API',
+    description='HBnB RESTful API v1',
+    doc='/docs'  # Swagger documentation at /api/v1/docs
+)
+
+# Import namespaces
+from .users import api as users_ns
+from .places import api as places_ns
+from .reviews import api as reviews_ns
+from .amenities import api as amenities_ns
+
+# Add namespaces to API
+api.add_namespace(users_ns, path='/users')
+api.add_namespace(places_ns, path='/places')
+api.add_namespace(reviews_ns, path='/reviews')
+api.add_namespace(amenities_ns, path='/amenities')
+
+# Export blueprint
+__all__ = ['bp']
