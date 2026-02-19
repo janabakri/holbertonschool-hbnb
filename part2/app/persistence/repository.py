d7e7b00298c17d6e639c83fd3d44c44dadf6b63d
jana <bkeyh382@gmail.com>
2026-02-19 04:19:47 +0300

    Refactor InMemoryRepository methods and cleanup

--- part2/app/persistence/repository.py
+++ part2/app/persistence/repository.py
@@ -1,50 +1,43 @@
-"""
-In-memory repository implementation
-"""
-
 class InMemoryRepository:
-    """In-memory storage for objects"""
-    
     def __init__(self):
-        """Initialize empty storage"""
         self.storage = {}
-    
+
     def add(self, obj):
         """Add object to storage"""
+        if not hasattr(obj, 'id'):
+            return None
         self.storage[obj.id] = obj
         return obj
-    
+
     def get(self, obj_id):
         """Get object by ID"""
         return self.storage.get(obj_id)
-    
+
     def get_all(self):
         """Get all objects"""
         return list(self.storage.values())
-    
+
     def update(self, obj_id, data):
-        """Update object with new data"""
+        """Update object"""
         obj = self.get(obj_id)
         if not obj:
             return None
-        
         for key, value in data.items():
             if hasattr(obj, key):
                 setattr(obj, key, value)
-        
-        if hasattr(obj, 'save'):
-            obj.save()
-        
         return obj
-    
+
     def delete(self, obj_id):
-        """Delete object from storage"""
-        return self.storage.pop(obj_id, None)
-    
+        """Delete object"""
+        if obj_id in self.storage:
+            del self.storage[obj_id]
+            return True
+        return False
+
     def exists(self, obj_id):
         """Check if object exists"""
         return obj_id in self.storage
-    
-    def clear(self):
-        """Clear all storage (for testing)"""
-        self.storage = {}
+
+    def count(self):
+        """Get count of objects"""
+        return len(self.storage)
