e1b66d33c2477d41cc7915594221e25a8e6c14c5
csrama <156553209+csrama@users.noreply.github.com>
2026-02-20 19:46:28 +0300

    Refactor API initialization and namespace paths

--- part2/app/api/v1/__init__.py
+++ part2/app/api/v1/__init__.py
@@ -1,17 +1,17 @@
 from flask_restx import Api
-from flask import Blueprint
-
-bp = Blueprint('api', __name__, url_prefix='/api/v1')
-api = Api(bp, version='1.0', title='HBnB API', description='HBnB RESTful API v1')
-
 from .users import api as users_ns
 from .places import api as places_ns
 from .reviews import api as reviews_ns
 from .amenities import api as amenities_ns
 
-api.add_namespace(users_ns, path='/users')
-api.add_namespace(places_ns, path='/places')
-api.add_namespace(reviews_ns, path='/reviews')
-api.add_namespace(amenities_ns, path='/amenities')
+api = Api(
+    title='HBnB API',
+    version='1.0',
+    description='HBnB Application API',
+    doc='/api/v1/'
+)
 
-__all__ = ['bp']
+api.add_namespace(users_ns, path='/api/v1/users')
+api.add_namespace(places_ns, path='/api/v1/places')
+api.add_namespace(reviews_ns, path='/api/v1/reviews')
+api.add_namespace(amenities_ns, path='/api/v1/amenities')
