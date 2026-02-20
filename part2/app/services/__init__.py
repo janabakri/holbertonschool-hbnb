391f758b306aea3fe503f024fccc40af1150022e
csrama <156553209+csrama@users.noreply.github.com>
2026-02-20 19:43:41 +0300

    Refactor __init__.py to remove facade instance
    Remove the singleton instance of HBnBFacade and update __all__.

--- part2/app/services/__init__.py
+++ part2/app/services/__init__.py
@@ -1,8 +1,3 @@
-#!/usr/bin/python3
-"""
-Shared Facade instance (singleton) for the entire app.
-All API modules must import this to use the same in-memory repositories.
-"""
 from app.services.facade import HBnBFacade
 
-facade = HBnBFacade()
+__all__ = ['HBnBFacade']
