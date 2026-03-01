4696f2c508dbf2dffab7275d1978c5fe4e7a2846
jana <bkeyh382@gmail.com>
2026-03-01 12:02:34 +0300

    Update review_repository.py

--- part3/app/ persistence/review_repository.py
+++ part3/app/ persistence/review_repository.py
@@ -1,1 +1,12 @@
+from app.persistence.base_repository import SQLAlchemyRepository
+from app.models.review import Review
 
+class ReviewRepository(SQLAlchemyRepository):
+    def __init__(self):
+        super().__init__(Review)
+    
+    def get_by_place(self, place_id):
+        return self.model.query.filter_by(place_id=place_id).all()
+    
+    def get_by_user(self, user_id):
+        return self.model.query.filter_by(user_id=user_id).all()
