9a7ab0c360f341527b5b07f93b9894ab3b2b5c9e
jana <bkeyh382@gmail.com>
2026-03-01 11:58:43 +0300

    Update place_amenity.py

--- part3/app/models/place_amenity.py
+++ part3/app/models/place_amenity.py
@@ -1,1 +1,16 @@
+from app.extensions import db
+from app.models.base_model import BaseModel
 
+class Amenity(BaseModel):
+    __tablename__ = 'amenities'
+    
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(200))
+    
+    def to_dict(self):
+        base_dict = super().to_dict()
+        base_dict.update({
+            'name': self.name,
+            'description': self.description
+        })
+        return base_dict
