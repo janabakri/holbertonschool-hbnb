b734380fdf70521903c653d62809cc7cc2decd99
jana <bkeyh382@gmail.com>
2026-03-01 12:01:28 +0300

    Update user_repository.py

--- part3/app/ persistence/user_repository.py
+++ part3/app/ persistence/user_repository.py
@@ -1,1 +1,9 @@
+from app.persistence.base_repository import SQLAlchemyRepository
+from app.models.user import User
 
+class UserRepository(SQLAlchemyRepository):
+    def __init__(self):
+        super().__init__(User)
+    
+    def get_by_email(self, email):
+        return self.model.query.filter_by(email=email).first()
