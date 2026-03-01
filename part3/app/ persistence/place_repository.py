76c6a1cb927e831e704646a88db93373aa103e4d
jana <bkeyh382@gmail.com>
2026-03-02 00:43:00 +0300

    Delete part3/app/ persistence/place_repository.py

--- part3/app/ persistence/place_repository.py
+++
@@ -1,9 +0,0 @@
-from app.persistence.base_repository import SQLAlchemyRepository
-from app.models.place import Place
-
-class PlaceRepository(SQLAlchemyRepository):
-    def __init__(self):
-        super().__init__(Place)
-    
-    def get_by_owner(self, owner_id):
-        return self.model.query.filter_by(owner_id=owner_id).all()
