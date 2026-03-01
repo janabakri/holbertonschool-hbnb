97e8e776e01d939f6f2b3e062ccf5b2b3e87a42b
jana <bkeyh382@gmail.com>
2026-03-01 12:02:59 +0300

    Update amenity_repository.py

--- part3/app/ persistence/amenity_repository.py
+++ part3/app/ persistence/amenity_repository.py
@@ -1,1 +1,6 @@
+from app.persistence.base_repository import SQLAlchemyRepository
+from app.models.amenity import Amenity
 
+class AmenityRepository(SQLAlchemyRepository):
+    def __init__(self):
+        super().__init__(Amenity)
