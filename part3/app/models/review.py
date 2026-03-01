9b04db63500e8cc18a2f42d0afb5b0b7ffd23e66
jana <bkeyh382@gmail.com>
2026-03-01 11:58:18 +0300

    Update review.py

--- part3/app/models/review.py
+++ part3/app/models/review.py
@@ -1,1 +1,20 @@
+from app.extensions import db
+from app.models.base_model import BaseModel
 
+class Review(BaseModel):
+    __tablename__ = 'reviews'
+    
+    text = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    user_id = db.Column(db.String(36), db.ForeignKey('users.id'), nullable=False)
+    place_id = db.Column(db.String(36), db.ForeignKey('places.id'), nullable=False)
+    
+    def to_dict(self):
+        base_dict = super().to_dict()
+        base_dict.update({
+            'text': self.text,
+            'rating': self.rating,
+            'user_id': self.user_id,
+            'place_id': self.place_id
+        })
+        return base_dict
