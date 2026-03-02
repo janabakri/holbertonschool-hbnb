7e401eac1d40ffb7b895ca80d3c1984e19e2f380
csrama <156553209+csrama@users.noreply.github.com>
2026-03-03 01:15:08 +0300

    Update place_amenity.py

--- part3/app/models/place_amenity.py
+++ part3/app/models/place_amenity.py
@@ -1,14 +1,7 @@
-"""Association table for many-to-many relationship between Place and Amenity"""
-from hbnb.app import db
 from app.extensions import db
 
 place_amenity = db.Table(
     "place_amenity",
     db.Column("place_id", db.String(36), db.ForeignKey("places.id")),
     db.Column("amenity_id", db.String(36), db.ForeignKey("amenities.id"))
-)
-# Association table for many-to-many relationship
-place_amenity = db.Table('place_amenity',
-    db.Column('place_id', db.String(36), db.ForeignKey('places.id'), primary_key=True),
-    db.Column('amenity_id', db.String(36), db.ForeignKey('amenities.id'), primary_key=True)
 )
