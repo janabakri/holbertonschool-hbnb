4036e6028973426fc49aae0726e11fd6417c3f89
csrama <156553209+csrama@users.noreply.github.com>
2026-03-03 01:45:32 +0300

    Update users.py

--- part3/app/api/v1/users.py
+++ part3/app/api/v1/users.py
@@ -1,6 +1,23 @@
-from flask_jwt_extended import get_jwt
+from flask_restx import Namespace, Resource, fields
+from flask import request
+from app.models.user import User
+from app.services.facade import HBnBFacade
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
-claims = get_jwt()
+api = Namespace('users', description='User operations')
+facade = HBnBFacade()
 
-if not claims.get("is_admin"):
-    return {"error": "Admins only"}, 403
+user_model = api.model('User', {
+    'first_name': fields.String(required=True),
+    'last_name': fields.String(required=True),
+    'email': fields.String(required=True),
+    'password': fields.String(required=True)
+})
+
+@api.route('/')
+class UserList(Resource):
+    @api.expect(user_model)
+    def post(self):
+        data = request.get_json()
+        user = facade.create_user(**data)
+        return user.to_dict(), 201
