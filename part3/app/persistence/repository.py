f5dee71c8d9d3cce3a20ceaa69097b4262ff8304
csrama <156553209+csrama@users.noreply.github.com>
2026-03-03 11:42:34 +0300

    Update repository.py

--- part3/app/persistence/repository.py
+++ part3/app/persistence/repository.py
@@ -1,18 +1,45 @@
 from app.models.user import User
+from app.models.place import Place
+from app.models.review import Review
+from app.models.amenity import Amenity
 from app import db
 
 
 class UserRepository:
-    @staticmethod
-    def get_by_email(email):
+    def get_by_email(self, email):
         return User.query.filter_by(email=email).first()
 
-    @staticmethod
-    def get_by_id(user_id):
+    def get_by_id(self, user_id):
         return User.query.get(user_id)
 
-    @staticmethod
-    def create(user):
+    def create(self, user):
         db.session.add(user)
         db.session.commit()
         return user
+
+
+class PlaceRepository:
+    def get_all(self):
+        return Place.query.all()
+
+    def get_by_id(self, place_id):
+        return Place.query.get(place_id)
+
+    def create(self, place):
+        db.session.add(place)
+        db.session.commit()
+        return place
+
+
+class ReviewRepository:
+    def create(self, review):
+        db.session.add(review)
+        db.session.commit()
+        return review
+
+
+class AmenityRepository:
+    def create(self, amenity):
+        db.session.add(amenity)
+        db.session.commit()
+        return amenity
