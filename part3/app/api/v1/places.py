2627dc8478c48eefc76c60fd9eda3cf306e43964
Raghad717 <r4gh4dalmalki@gmail.com>
2026-03-10 01:18:37 +0300

    Add GET method to retrieve all places

--- part3/app/api/v1/places.py
+++ part3/app/api/v1/places.py
@@ -13,10 +13,19 @@ place_model = api.model('Place', {
 
 @api.route('/')
 class PlaceList(Resource):
+
+    def get(self):
+        places = facade.get_all_places()
+        return [p.to_dict() for p in places], 200
+
     @jwt_required()
     @api.expect(place_model)
     def post(self):
         user_id = get_jwt_identity()
         data = request.get_json()
+
+        if not data:
+            return {"error": "Invalid input"}, 400
+
         place = facade.create_place(user_id=user_id, **data)
         return place.to_dict(), 201
